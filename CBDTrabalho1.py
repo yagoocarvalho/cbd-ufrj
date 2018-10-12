2468748fceb274f0ddd6cde7e4b4db579127354f
bconfessor <brian_confessor@poli.ufrj.br>
2018-10-11 23:47:09 -0300

    Merged files into CDBTrabalho1.py; Created MakeHEAD and UpdateHEAD methods

--- CBDTrabalho1.py
+++ CBDTrabalho1.py
@@ -26,22 +26,25 @@ MGPath = CandidatesFilePath + "MG.csv"
 #caminho dos arquivos 
 BDFilePath = "C:/Users/BRC/Downloads/consulta_cand_2018/BD/"
 HeapPath = BDFilePath + "HeapBD.txt"
+HeapHeadPath = BDFilePath + "HeapHEAD.txt"
 OrderedPath = BDFilePath + "OrderedBD.txt"
+OrderedHeadPath = BDFilePath + "OrderedHEAD.txt"
 HashPath = BDFilePath + "HashBD.txt"
+HashHeadPath = BDFilePath+ "HashHEAD.txt"
 
-#caracter usado como enchimento de valores não-cheios de um registro
+#caracter usado como enchimento de valores nao-cheios de um registro
 paddingCharacter = "#"
 #tamanho de um registro(medido em caracteres)
 registrySize = 153
 
 
-#Tamanho de um bloco de memória (medido em registros)
+#Tamanho de um bloco de memoria (medido em registros)
 blockSize = 5
 
 #Tamanho do head(em linhas)
 heapHeadSize = 5
 
-#Tamanhos máximos de cada atributo(for reference mostly)
+#Tamanhos maximos de cada atributo(for reference mostly)
 dicColunaTamanhoMax = {
 	"K": 2,
 	"N": 2,
@@ -85,7 +88,7 @@ colHeadersList = ["CPF", "SG_UF", "CD_CARGO", 'NR_CANDIDATO', 'NM_CANDIDATO', 'N
 #Baseado nos indices acima
 relevantColsList = [10, 13, 16, 17, 20, 21, 27, 38, 41, 43, 45, 47, 49]
 
-#é uma coluna relevante dentro do Excel(baseado nas colunas escolhidas acima)
+#retorna se e uma coluna relevante dentro do Excel(baseado nas colunas escolhidas acima)
 def isRelevantRow(rowNumber):
     #
     return rowNumber in relevantColsList
@@ -98,9 +101,9 @@ def calculateRegistrySize():
     return sum
 
 
-#Preenche com 0's à esquerda CPFs que não possuem seu tamanho totalmente preenchido
+#Preenche com 0's a esquerda CPFs que nao possuem seu tamanho totalmente preenchido
 def fillCPF(cpf):
-    return cpf.zfill(maxColSizesList[0])#tamanho de CPF é fixo
+    return cpf.zfill(maxColSizesList[0])#tamanho de CPF e fixo
 
 #Completa a string com caracter escolhido para padding(p/ manter tamanho fixo)
 def padString(stringToPad, totalSizeOfField):
@@ -111,7 +114,7 @@ def padString(stringToPad, totalSizeOfField):
 
 
 #Lê o arquivo desejado e retorna uma lista com todos os registros relevantes do mesmo
-#lista retornada será usada para construir nossos proprios arquivos
+#lista retornada sera usada para construir nossos proprios arquivos
 def readFromFile(csvFilePath):
     lineCount = 0
     registros = []
@@ -125,7 +128,7 @@ def readFromFile(csvFilePath):
                 
                 for i in range(len(row)):
                     if isRelevantRow(i):
-                        #Se é a coluna do CPF, coloca o mesmo no inicio da lista
+                        #Se for a coluna do CPF, coloca o mesmo no inicio da lista
                         if i == relevantColsList[4]:
                             finalRow.insert(0, fillCPF(row[i]))
                         else:
@@ -183,28 +186,71 @@ def deleteLineFromFile(location, filepath):
             # write line in the output file
             print(line)
 
-def MakeHEAD(headType):
+
+def MakeHEAD(headType, numRegistries):
     string = "File structure: " + headType + "\n"
     string += "Creation: " + datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') + "\n"
     string += "Last modification: " + datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') + "\n"
     string += "Schema: "
     for key, value in dicColHeaderType.items():
         string += key + "-" + value + "|"
-    string += "\nNumber of registries: 0\n"
+    string += "\nNumber of registries: " + str(numRegistries) + "\n"
     
     return string
 
 
+def MakeHEAD2(headPath, headType, numRegistries):
+    file = open(headPath, 'a')
+    string = "File structure: " + headType + "\n"
+    string += "Creation: " + datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') + "\n"
+    string += "Last modification: " + datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') + "\n"
+    string += "Schema: "
+    for key, value in dicColHeaderType.items():
+        string += key + "-" + value + "|"
+    string += "\nNumber of registries: " + str(numRegistries) + "\n"
+    file.write(string)
+    #return string
+
+
+
+def UpdateHEADFile(headPath, numRegistries):
+    file = open(headPath, 'r')
+    
+    headContent = file.readlines()
+    print(headContent)
+    headContent
+    file.close()
+    os.remove(headPath)
+    
+    #recria ela com as alteracoes
+    file = open(headPath, 'a')
+    file.write(headContent[0])
+    file.write(headContent[1])
+    file.write("Last modification: " + datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') + "\n")
+    file.write(headContent[3])
+    file.write("\nNumber of registries: " + str(numRegistries) + "\n")
+
+
+#Gets number of registries from HEAD file
+def getNumRegistries(DBHeadFilePath, headSize):
+    #posiÃ§Ã£o de inÃ­cio de leitura dos dados
+    #cursorBegin = startingR
+    with open(DBHeadFilePath, 'r') as file:
+        for i in range(headSize-1):
+            file.readline()
+        return (int(file.readline().split("Number of registries: ")[1]))
+
+
 #StartingRegistry = index do registro inicial a ser buscado (0-based)
 def FetchBlock(DBFilePath, startingRegistry):
-    #posição de início de leitura dos dados
+    #posicao de inicio de leitura dos dados
     #TODO
     #cursorBegin = startingR
     block = []
     with open(DBFilePath, 'r') as file:
-        #Pula o HEAD
-        for i in range(heapHeadSize):
-            file.readline()#HEAD possui tamanho variável, então pulamos a linha inteira
+        #Pula o HEAD(UPDATE: HEAD is in another cast....file)
+        #for i in range(heapHeadSize):
+        #    file.readline()#HEAD possui tamanho variável, então pulamos a linha inteira
             #Em termos de BD, seria o análogo à buscar o separador de registros, nesse caso, '\n'
         
         #Em seguida, move o ponteiro do arquivo para a posição correta(offset)
@@ -388,10 +434,164 @@ def HeapInsertSingleRecord(listOfValues):
 ###################################################################################
 ################################### ORDERED #######################################
 ###################################################################################
+from dateutil import parser
+import math
+
+# Campo nao chave para ordenacao
+numColToOrder = 0
+isOrderedByPrimaryKey = True
+
+
+#Le o CSV e cria o arquivo do BD Ordenado
+def CreateOrderedBD(csvFilePath, isOrderedByPrimaryKey):
+    #Lê do CSV e preenche os registros com enchimento para criar o tamanho fixo
+    valuesToLoad = padRegistries(readFromFile(csvFilePath))
+    valuesToLoad = sortList(valuesToLoad, isOrderedByPrimaryKey)
+    #apaga o conteúdo existente no momento(se houver)
+    if os.path.exists(OrderedPath):
+        os.remove(OrderedPath)
+    
+    #preenche os valores direto no arquivo
+    file = open(OrderedPath, "w+")
+    file.write(MakeHEAD("Ordered", len(valuesToLoad)))
+    for row in valuesToLoad:
+        for cols in row:
+            file.write(cols)
+    
+    file.close()
+
+# Funcao para auxiliar na ordenacao
+def sortComparison(elem):
+    # Se o campo for de data, converte-lo para este formato
+    if(numColToOrder == 7):
+        return parser.parse(elem[numColToOrder])
+    #Outros campos
+    else:
+        return elem[numColToOrder]
+
+# Ordena um array de registros
+def sortList(values, isPrimaryKey):
+    if (isPrimaryKey is False):
+        return sorted(values, key=sortComparison)
+    else:
+        return sorted(values)
+
 
 ###################################################################################
 ########################## ORDERED - SELECT FUNCTIONS #############################
 ###################################################################################
+
+# TODO consertar busca em data
+# Retorna o bloco se o achar, ou -1, caso contrario; e o numero de blocos utilizados
+def binarySearch(columnIndex, value, maxNumBlocks, singleRecordSelection = False):
+    #conta o número de vezes que "acessamos a memória do disco"
+    numberOfBlocksUsed = []
+    
+    # blocos encontrados na query    
+    foundedBlocks=[]
+    
+    # blocos acessados durante a query
+    accessedBlocks=[]
+    
+    # intervalo de procura dos blocos
+    l = 0
+    r = maxNumBlocks
+    while l <= r: 
+        # Pega o numero do bloco do meio
+        mid = math.ceil(l + (r - l)/2); 
+                
+        # 0-based
+        # Busca o registro
+        blockRegistries = FetchBlock(OrderedPath, (mid-1)*5)
+        
+        getNearBlocks(mid, foundedBlocks,columnIndex, value, accessedBlocks,
+                      maxNumBlocks, numberOfBlocksUsed, singleRecordSelection)
+#                    
+        # Check if x is present at mid 
+        if (foundedBlocks):         
+            return foundedBlocks, len(numberOfBlocksUsed)
+        else:
+            # Se o valor é maior que o ultimo elemento, ignora a metade esquerda  
+            if(value > blockRegistries[-1][columnIndex]):
+                l = mid + 1
+            # Se o valor é menor, ignorar metade direita
+            else:
+                r = mid - 1
+                
+    # Retorna -1 se não achar
+    return -1, len(numberOfBlocksUsed)
+
+
+def getNearBlocks(numberBlock, foundedBlocks,columnIndex, value, accessedBlocks,
+                  maxNumBlocks, numberOfBlocksUsed, singleRecordSelection = False):
+    
+    if(numberBlock not in accessedBlocks):
+        accessedBlocks.append(numberBlock)
+        numberOfBlocksUsed.append('1')
+        
+        # array de indices de blocos encontrados
+        indexesFoundedBlocks = []
+        
+        # recupera o bloco de registros
+        blockRegistries = FetchBlock(OrderedPath, (numberBlock-1)*5)
+        
+        # Varre cada registro do bloco procurando o valor
+        for idx, block in enumerate(blockRegistries):
+            if value in block[columnIndex]:
+                if(singleRecordSelection):
+                    foundedBlocks.append(block)
+                    return
+                indexesFoundedBlocks.append(idx)
+                foundedBlocks.append(block)
+        
+        # se contem o indice 0 é possivel que tenha mais registros no bloco anterior
+        if( (0 in indexesFoundedBlocks) and numberBlock>1):
+            getNearBlocks(numberBlock-1, foundedBlocks,
+                columnIndex, value, accessedBlocks,maxNumBlocks, numberOfBlocksUsed)
+    
+        # se contem o indice 4 é possivel que tenha mais registros no proximo bloco
+        if((4 in indexesFoundedBlocks) and numberBlock<maxNumBlocks):
+            getNearBlocks(numberBlock+1, foundedBlocks, 
+                    columnIndex, value, accessedBlocks, maxNumBlocks, numberOfBlocksUsed)
+
+    
+
+
+#colName = Desired column of the query (SEE LISTS ABOVE FOR COL NAMES)
+#value = desired value
+#SQL Format: Select * from OrderedTable WHERE colName = value
+#Retorna o PRIMEIRO registro onde o colName tenha o valor igual à value
+def OrderedSelectSingleRecord(colName, value):
+    numberOfBlocksUsed = 0 #conta o número de vezes que "acessamos a memória do disco"
+    
+    if colName not in colHeadersList:
+        print("Error: Column name not found in relation.")
+        return
+    columnIndex = colHeadersList.index(colName) #pega o indice referente àquela coluna
+
+    print("Running query: ")
+    print("SELECT * FROM TB_ORDERED WHERE " + colName + " = " + value + ";")
+
+    # Obtem o numero de blocos do BD
+    numBlocks = math.ceil(getNumRegistries(OrderedPath)/blockSize)
+    
+    # Verifica se o campo procurado eh equivalente ao campo pelo qual o banco foi ordenado
+    # Caso seja, utilizar busca binaria
+    if(columnIndex == numColToOrder):   
+        blockFounded,numberOfBlocksUsed = binarySearch(numColToOrder, value,
+                                                       numBlocks, True)
+    # Senao realizar select linear
+    else:
+        blockFounded = None #fazer select normal
+        
+    if(blockFounded):
+        print("Registro encontrado: ")
+        print(blockFounded)
+    else:
+        print("Registro não encontrado")
+
+    print("Fim da busca.")
+    print("Número de blocos varridos: " + str(numberOfBlocksUsed))
 
 ###################################################################################
 ########################## ORDERED - INSERT FUNCTIONS #############################
@@ -400,3 +600,30 @@ def HeapInsertSingleRecord(listOfValues):
 ###################################################################################
 ########################## ORDERED - DELETE FUNCTIONS #############################
 ###################################################################################
+
+
+
+###################################################################################
+################################### MAIN ##########################################
+###################################################################################
+
+
+#CreateOrderedBD(RJPath, False)
+
+#print('-----')
+
+#blocks = binarySearch(1)
+
+#query = "86551337791"
+#p = [block if "09650185712" in block[numColToOrder] else None for block in blocks ]
+#q = next((block for block in blocks if query in block[numColToOrder]), None)
+
+#print(q)
+
+#0, len(arr)-1,
+#result = binarySearch(numColToOrder, query, 4)
+#print (result)
+
+#OrderedSelectSingleRecord('NM_EMAIL', query)
+#OrderedSelectSingleRecord(colHeadersList[numColToOrder], query)
+
